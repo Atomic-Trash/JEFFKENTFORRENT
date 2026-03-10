647c6533307a78714a7bc0b8091b55130ac01755
Atomic-Trash <jeffreyparkerkent@gmail.com>
2026-03-09 18:26:19 -0700

    Optimize website: compress images, preload hero asset, remove unused font

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
   root: '.',
+  plugins: [
+    ViteImageOptimizer(),
+  ],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
